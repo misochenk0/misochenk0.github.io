e62a200bdda59f38f6c25e96d2e2e1720840e5d5
misochenk0 <78084319+misochenk0@users.noreply.github.com>
2021-03-28 23:16:30 +0300

    Add files via upload

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,48 @@
-$(document).ready(function(){
-    $('.portfolio__carousel').slick({
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const menu = document.querySelector('.header__menu'),
+          button = document.querySelector('.button_transparent'),
+          title = document.querySelector('.header__title'),
+          subTitle = document.querySelector('.header__subtitle'),
+          body = document.querySelector('body');
+
+    addBottomClass = function(item) {
+        item.classList.add('bottom');
+    };
+    addShowedClass = function(item) {
+        item.classList.add('showed');
+    };
+
+    setTimeout(addBottomClass, 3300, menu);
+    setTimeout(addBottomClass, 2300, button);
+    setTimeout(addShowedClass, 1000, title);
+    setTimeout(addShowedClass, 2300, subTitle);
+    setTimeout(function(item) {
+        item.classList.add('visible');
+    }, 4000, body);
+
+    $(document).ready(function(){
+        $('.portfolio__carousel').slick({
+        });
+    });
+    $(document).ready(function(){
+        $("#header, #about, #skills, #portfolio").on("click","a", function (event) {
+            event.preventDefault();
+            const id  = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+    });
+
+    
+    
+    document.addEventListener('scroll', function() {
+        if (pageYOffset > 200) {
+            menu.classList.add('active');
+        } else {
+            menu.classList.remove('active');
+        }
     });
 });
-$(document).ready(function(){
-    $("#header, #about, #skills, #portfolio").on("click","a", function (event) {
-        event.preventDefault();
-        const id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
-});
+
