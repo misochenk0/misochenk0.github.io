cf597c4eaa12d75f0a33ccb717aacb0135f4a466
misochenk0 <78084319+misochenk0@users.noreply.github.com>
2021-03-28 23:19:18 +0300

    Add files via upload

--- dist/js/script.js
+++ dist/js/script.js
@@ -35,7 +35,21 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $("form").trigger("reset");
+        });
+        return false;
+    });
     
     document.addEventListener('scroll', function() {
         if (pageYOffset > 200) {
