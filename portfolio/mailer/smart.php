23f0f9f415bc687c20507db3c66b7afb350155c1
misochenk0 <78084319+misochenk0@users.noreply.github.com>
2021-04-05 12:34:17 +0300

    Update smart.php

--- portfolio/mailer/smart.php
+++ portfolio/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'soup20147@gmail.com';                 // Наш логин
-$mail->Password = '787049sasham';                           // Наш пароль от ящика
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
